63306047ee4094d1ed6be7bdaf88673c55bfb5d4
jaynuevo <mart_tin97@yahoo.com>
2017-03-25 15:52:47 +0800

    Update Link.java

--- Link.java
+++ Link.java
@@ -1,15 +1,25 @@
 public class Link {
 	public String op;
-
+	public double num;
 	public Link next;
+	String c,d;
 	
-	
-	public Link(String operator) {
-		op = operator;
+	public Link(String string) {
+		
+			op = string;
+		
 	}
 	
 	public String getOp(){
 		return op;
+	}
+	
+	public double getNum(){
+		return num;
+	}
+	
+	public void setNum(){
+		num = Double.parseDouble(op);
 	}
 	
 	public void show(){
