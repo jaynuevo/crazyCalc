17b098ddcb68edf2f55603033889de36d89f4787
jaynuevo <mart_tin97@yahoo.com>
2017-03-28 08:53:33 +0800

    Update CrazyTest.java

--- CrazyTest.java
+++ CrazyTest.java
@@ -6,7 +6,7 @@ public class CrazyTest {
 		CrazyCalc c = new CrazyCalc();
 		
 		c.setVisible(true);
-		c.setSize(400, 150);
+		c.setSize(380, 500);
 		c.setLocationRelativeTo(null);
 		c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		c.setResizable(false);
